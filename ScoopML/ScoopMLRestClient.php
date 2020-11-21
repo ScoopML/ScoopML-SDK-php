21a6d33aa8d0c8d8ac3323a9846c323ec2074663
Harish S.G <harishsg99@gmail.com>
2020-11-21 23:09:51 +0530

    Create ScoopMLRestClient.php

---
+++ ScoopML/ScoopMLRestClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ScoopML\ScoopMLRestClient;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Command\Guzzle\Description;
+use GuzzleHttp\Command\Guzzle\GuzzleClient;
+
+class ScoopMLRestClient extends GuzzleClient {
+
+    public static function create($config = []) {
+        // Load the service description file.
+        $service_description = new Description(
+            ['baseUrl' => $config['base_uri']] + (array) json_decode(file_get_contents(__DIR__ . '/../service.json'), TRUE)
+        );
+
+        // Creates the client and sets the default request headers.
+        $client = new Client([
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ],
+            'auth' =>  [$config['api_user']],
+        ]);
+
+        return new static($client, $service_description, NULL, NULL, NULL, $config);
+    }
+}
